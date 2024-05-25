60164775d783d6f6d64e2136916ef782e6fb07ec
Ismael Begara <isbergara1@gmail.com>
2024-05-24 23:25:05 -0300

    Create Agenda.php

---
+++ App/Models/Agenda.php
@@ -0,0 +1,50 @@
+<?php
+
+class Agenda {
+    private $horaInicio;
+    private $dias;
+    private $fecha;
+    private $horaFin;
+
+    public function __construct($horaInicio, $dias, $fecha, $horaFin) {
+        $this->horaInicio = $horaInicio;
+        $this->dias = $dias;
+        $this->fecha = $fecha;
+        $this->horaFin = $horaFin;
+    }
+
+    // Getters
+    public function getHoraInicio() {
+        return $this->horaInicio;
+    }
+
+    public function getDias() {
+        return $this->dias;
+    }
+
+    public function getFecha() {
+        return $this->fecha;
+    }
+
+    public function getHoraFin() {
+        return $this->horaFin;
+    }
+
+    // Setters
+    public function setHoraInicio($horaInicio) {
+        $this->horaInicio = $horaInicio;
+    }
+
+    public function setDias($dias) {
+        $this->dias = $dias;
+    }
+
+    public function setFecha($fecha) {
+        $this->fecha = $fecha;
+    }
+
+    public function setHoraFin($horaFin) {
+        $this->horaFin = $horaFin;
+    }
+}
+?>
