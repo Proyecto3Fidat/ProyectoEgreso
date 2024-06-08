40cc047c9864f0d17c37e481ea282484a0c020ed
Ismael Begara <isbergara1@gmail.com>
2024-06-07 22:31:06 -0300

    a

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'ismael/proyecto-egreso',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '6948a18e8c423afa90feb0c81c9c7dd7328e3e33',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'ismael/proyecto-egreso' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '6948a18e8c423afa90feb0c81c9c7dd7328e3e33',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'pecee/simple-router' => array(
+            'pretty_version' => '5.4.1.7',
+            'version' => '5.4.1.7',
+            'reference' => 'a2843d5b1e037f8b61cc99f27eab52a28bf41dfd',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../pecee/simple-router',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
