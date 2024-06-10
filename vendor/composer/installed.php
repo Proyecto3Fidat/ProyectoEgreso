c54fd0e6568bfcd9954c3916db738310018bbd0f
Ismael Begara <isbergara1@gmail.com>
2024-06-10 13:01:33 -0300

    a

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,9 +1,9 @@
 <?php return array(
     'root' => array(
         'name' => 'ismael/proyecto-egreso',
-        'pretty_version' => 'dev-main',
-        'version' => 'dev-main',
-        'reference' => '473d05ef04384503f939526f289a70ffec619513',
+        'pretty_version' => '1.0.0+no-version-set',
+        'version' => '1.0.0.0',
+        'reference' => null,
         'type' => 'project',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -11,9 +11,9 @@
     ),
     'versions' => array(
         'ismael/proyecto-egreso' => array(
-            'pretty_version' => 'dev-main',
-            'version' => 'dev-main',
-            'reference' => '473d05ef04384503f939526f289a70ffec619513',
+            'pretty_version' => '1.0.0+no-version-set',
+            'version' => '1.0.0.0',
+            'reference' => null,
             'type' => 'project',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
