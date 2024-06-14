639c6f101609630286b0e9607b2e0f2d521d535c
Ismael Begara <isbergara1@gmail.com>
2024-06-13 21:55:43 -0300

    limpiar rama

--- Public/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-require_once '../vendor/autoload.php';
-
-use Pecee\SimpleRouter\SimpleRouter;
-use App\Controllers\HomeController;
-require_once '../Routes/web.php';
-
-SimpleRouter::start();
