22ff519b382b2dccf232d133faf9f0d11126d37d
Ismael Begara <isbergara1@gmail.com>
2024-06-29 00:29:05 -0300

    logger

--- App/Repositories/ClienteRepository.php
+++ App/Repositories/ClienteRepository.php
@@ -1,64 +1,55 @@
 <?php
+
 namespace App\Repositories;
 
 use App\Models\ClienteModel;
-use App\Repositories\Database;
-use App\Repositories\Logger;
 
 class ClienteRepository {
     private $database;
-    private $logger;
 
     public function __construct() {
         $this->database = new Database();
-        $this->logger = new Logger(); // Inicializar el logger
     }
 
     public function guardar(ClienteModel $clienteModel){
-        try {
-            $this->database->connect();
-                
-            $sql = "INSERT INTO Cliente (nroDocumento, tipoDocumento, altura, peso, calle, numero, esquina, email, patologias, fechaNacimiento, nombre, apellido) 
-                    VALUES (?,?, ?, ?, ?, ?, ?,  ?, ?, ?, ?, ?)";
+        $this->database->connect();
             
-            $nroDocumento = $clienteModel->getNroDocumento();
-            $tipoDocumento = $clienteModel->getTipoDocumento();
-            $altura = $clienteModel->getAltura();
-            $peso = $clienteModel->getPeso();
-            $calle = $clienteModel->getCalle();
-            $numero = $clienteModel->getNumero();
-            $esquina = $clienteModel->getEsquina();
-            $email = $clienteModel->getEmail();
-            $patologias = $clienteModel->getPatologias();
-            $fechaNacimiento = $clienteModel->getFechaNacimiento();
-            $nombre = $clienteModel->getNombre();
-            $apellido = $clienteModel->getApellido();
+        $sql = "INSERT INTO Cliente (nroDocumento, tipoDocumento, altura, peso, calle, numero, esquina, email, patologias, fechaNacimiento, nombre, apellido) 
+                VALUES (?,?, ?, ?, ?, ?, ?,  ?, ?, ?, ?, ?)";
+        
+        $nroDocumento = $clienteModel->getNroDocumento();
+        $tipoDocumento = $clienteModel->getTipoDocumento();
+        $altura = $clienteModel->getAltura();
+        $peso = $clienteModel->getPeso();
+        $calle = $clienteModel->getCalle();
+        $numero = $clienteModel->getNumero();
+        $esquina = $clienteModel->getEsquina();
+        $email = $clienteModel->getEmail();
+        $patologias = $clienteModel->getPatologias();
+        $fechaNacimiento = $clienteModel->getFechaNacimiento();
+        $nombre = $clienteModel->getNombre();
+        $apellido = $clienteModel->getApellido();
 
-            
-            $stmt = $this->database->getConnection()->prepare($sql);
-            $stmt->bind_param(
-                "isiisissssss",
-                $nroDocumento,
-                $tipoDocumento,
-                $altura,
-                $peso,
-                $calle,
-                $numero,
-                $esquina,
-                $email,
-                $patologias,
-                $fechaNacimiento,
-                $nombre,
-                $apellido,
-            );
-            $stmt->execute();
-            $stmt->close();
-            $this->database->disconnect(); 
-        } catch (\Exception $e) {
-            // Registrar el error utilizando el logger
-            $this->logger->logError("Error en ClienteRepository::guardar(): " . $e->getMessage());
-            die("Error al guardar cliente. Revisa el registro de errores.");
-        }
+        
+        $stmt = $this->database->getConnection()->prepare($sql);
+        $stmt->bind_param(
+            "isiisissssss",
+            $nroDocumento,
+            $tipoDocumento,
+            $altura,
+            $peso,
+            $calle,
+            $numero,
+            $esquina,
+            $email,
+            $patologias,
+            $fechaNacimiento,
+            $nombre,
+            $apellido,
+        );
+        $stmt->execute();
+        $stmt->close();
+        $this->database->disconnect(); 
     }
+    
 }
-?>
--- App/Repositories/UsuarioRepository.php
+++ App/Repositories/UsuarioRepository.php
@@ -1,109 +1,83 @@
 <?php
 namespace App\Repositories;
 
+if (session_status() == PHP_SESSION_NONE) {
+    session_start();
+}
+
 use App\Models\UsuarioModel;
-use App\Repositories\Database;
-use App\Repositories\Logger;
 
 class UsuarioRepository {
     private $database;
-    private $logger;
 
     public function __construct() {
         $this->database = new Database();
-        $this->logger = new Logger(); // Inicializar el logger
     }
-
     public function comprobarUsuario($nroDocumento) {
-        try {
-            $this->database->connect();
-            $sql = "SELECT nroDocumento FROM Usuario WHERE nroDocumento = ?";
-            $stmt = $this->database->getConnection()->prepare($sql);
-            $stmt->bind_param("i", $nroDocumento);
-            $stmt->execute();
-            $stmt->store_result();
-            $num_of_rows = $stmt->num_rows;
-            $stmt->close();
-            $this->database->disconnect();
-            return $num_of_rows > 0;
-        } catch (\Exception $e) {
-            // Registrar el error utilizando el logger
-            $this->logger->logError("Error en UsuarioRepository::comprobarUsuario(): " . $e->getMessage());
-            die("Error al comprobar usuario. Revisa el registro de errores.");
-        }
+        $this->database->connect();
+        $sql = "SELECT nroDocumento FROM Usuario WHERE nroDocumento = ?";
+        $stmt = $this->database->getConnection()->prepare($sql);
+        $stmt->bind_param("i", $nroDocumento);
+        $stmt->execute();
+        $stmt->store_result();
+        $num_of_rows = $stmt->num_rows;
+        $stmt->close();
+        $this->database->disconnect();
+        return $num_of_rows > 0;
     }
-
     public function guardar(UsuarioModel $usuarioModel){
-        try {
-            $this->database->connect();
-            $sql = "INSERT INTO Usuario (nroDocumento, rol, passwd) 
-                    VALUES (?,?, ?)";
-            
-            $nroDocumento = $usuarioModel->getNroDocumento();
-            $rol = $usuarioModel->getRol();
-            $passwd = $usuarioModel->getPasswd();       
-            $stmt = $this->database->getConnection()->prepare($sql);
-            $stmt->bind_param(
-                "iis",
-                $nroDocumento,
-                $rol,
-                $passwd
-            );
-            $stmt->execute();
-            $stmt->close();
-            $this->database->disconnect();
-        } catch (\Exception $e) {
-            // Registrar el error utilizando el logger
-            $this->logger->logError("Error en UsuarioRepository::guardar(): " . $e->getMessage());
-            die("Error al guardar usuario. Revisa el registro de errores.");
-        }
+        $this->database->connect();
+        $sql = "INSERT INTO Usuario (nroDocumento, rol, passwd) 
+                VALUES (?,?, ?)";
+        
+        $nroDocumento = $usuarioModel->getNroDocumento();
+        $rol = $usuarioModel->getRol();
+        $passwd = $usuarioModel->getPasswd();       
+        $stmt = $this->database->getConnection()->prepare($sql);
+        $stmt->bind_param(
+            "iis",
+            $nroDocumento,
+            $rol,
+            $passwd
+        );
+        $stmt->execute();
+        $stmt->close();
+        $this->database->disconnect();
     }
     
     public function autenticar($nroDocumento, $passwd) {
-        try {
-            $this->database->connect();
-            $sql = "SELECT passwd , nroDocumento FROM Usuario WHERE nroDocumento = ?";
-            $stmt = $this->database->getConnection()->prepare($sql);
-            $stmt->bind_param("i", $nroDocumento);
-            $stmt->execute();
-            if($stmt->error){
-                echo "error";
-            }
-            $stmt->bind_result($epasswd, $edocumento);
-            $stmt->fetch();
-            $stmt->close();
-            $this->database->disconnect();
-            if ($epasswd == $passwd || password_verify($passwd, $epasswd)){
-                return $this->nombreCliente($edocumento);
-            }
-            return false;
-        } catch (\Exception $e) {
-            // Registrar el error utilizando el logger
-            $this->logger->logError("Error en UsuarioRepository::autenticar(): " . $e->getMessage());
-            die("Error al autenticar usuario. Revisa el registro de errores.");
+        $this->database->connect();
+        $sql = "SELECT passwd , nroDocumento FROM Usuario WHERE nroDocumento = ?";
+        $stmt = $this->database->getConnection()->prepare($sql);
+        $stmt->bind_param("i", $nroDocumento);
+        $stmt->execute();
+        if($stmt->error){
+            echo "error";
         }
+        $stmt->bind_result($epasswd, $edocumento);
+        $stmt->fetch();
+        $stmt->close();
+        $this->database->disconnect();
+        if ($epasswd == $passwd || password_verify($passwd, $epasswd)){
+            return $this->nombreCliente($edocumento);
+        }
+        return false;
     }
 
     public function nombreCliente($documento){
-        try {
-            $this->database->connect();
-            $sql = "SELECT nombre  FROM Cliente WHERE nroDocumento = ?";
-            $stmt = $this->database->getConnection()->prepare($sql);
-            $stmt->bind_param("i", $documento);
-            $stmt->execute();
-            if($stmt->error){
-                echo "error";
-            }
-            $stmt->bind_result($nombre);
-            $stmt->fetch();
-            $stmt->close();
-            $this->database->disconnect();
-            return $nombre;
-        } catch (\Exception $e) {
-            // Registrar el error utilizando el logger
-            $this->logger->logError("Error en UsuarioRepository::nombreCliente(): " . $e->getMessage());
-            die("Error al obtener nombre del cliente. Revisa el registro de errores.");
+        $this->database->connect();
+        $sql = "SELECT nombre  FROM Cliente WHERE nroDocumento = ?";
+        $stmt = $this->database->getConnection()->prepare($sql);
+        $stmt->bind_param("i", $documento);
+        $stmt->execute();
+        if($stmt->error){
+            echo "error";
         }
+        $stmt->bind_result($nombre);
+        $stmt->fetch();
+        $stmt->close();
+        $this->database->disconnect();
+        return $nombre;
     }
 }
 ?>
