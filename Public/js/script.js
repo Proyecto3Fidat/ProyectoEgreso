02eafdc9d5b2e08183eaf3d499e08180f19ec332
Facundo_Recagno <facundoezequiel459@gmail.com>
2024-07-07 20:57:32 -0300

    jiji

--- Public/js/script.js
+++ Public/js/script.js
@@ -1,10 +1,20 @@
-document.addEventListener('DOMContentLoaded', function() {
-            const welcomeMessage = document.getElementById('welcome-message');
-            const documento = localStorage.getItem('documento');
-    
-            if (documento) {
-                welcomeMessage.innerHTML = `<a>Bienvenido, ${documento}</a>  <a href="/logout">Cerrar sesión</a>`;
-            } else {
-                welcomeMessage.innerHTML = '<a href="/login">Ingresar</a> <a href="../App/Views/crearUsuario.html">Registrarse</a>';
-            }
-        });
+// document.addEventListener('DOMContentLoaded', function() {
+//     const welcomeMessage = document.getElementById('welcome-message');
+//     const documento = localStorage.getItem('documento');
+
+//     if (documento) {
+//         welcomeMessage.innerHTML = `<a>Bienvenido, ${documento}</a>  <a href="/logout">Cerrar sesión</a>`;
+//     } else {
+//         welcomeMessage.innerHTML = '<a href="/login">Ingresar</a> <a href="../App/Views/crearUsuario.html">Registrarse</a>';
+//     }
+// });
+
+function validateForm() {
+    var altura = document.getElementById("altura").value;
+    var alturaPattern = /^\d(\.\d{1,2})?$/;
+    if (!alturaPattern.test(altura)) {
+        alert("La altura debe tener el formato correcto: 1.85");
+        return false;
+    }
+    return true;
+}
