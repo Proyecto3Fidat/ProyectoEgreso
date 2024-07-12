dee5fcfe2c3dc3ebc929ee4a7c314e17674c9d14
Ismael Begara <isbergara1@gmail.com>
2024-07-12 20:04:26 -0300

    Update ClienteService.php
    Descripcion y comando mejorado

--- App/Services/ClienteService.php
+++ App/Services/ClienteService.php
@@ -17,18 +17,24 @@ class ClienteService {
     public function crearAdministrador(ClienteModel $clienteModel) {
         $this->clienteRepository->guardarAdministrador($clienteModel);
     }
-    public function emailBienvenida($email){
+    public function emailBienvenida($email) {
         $para = $email;
         $asunto = "Bienvenido a la plataforma de entrenamiento";
         $mensaje = "Bienvenido a la plataforma de entrenamiento";
-
-        $comando = 'echo "' . addslashes($mensaje) . '" | mailx -s "' . addslashes($asunto) . '" ' . $para;
+        $from = "FIDAT <isbergara1@gmail.com>";
+    
+        // Comando para enviar correo usando mailx
+         $comando = 'echo ' . escapeshellarg($mensaje) . ' | mailx -s ' . escapeshellarg($asunto) . ' -S from=' . escapeshellarg($from) . ' ' . $para;
+        // Ejecutar el comando y capturar la salida
+        // echo "Bienvenido a la plataforma de entrenamiento" | mailx -s "Bienvenido a la plataforma de entrenamiento" -S from="FIDAT <isbergara1@gmail.com>" {$email}
         exec($comando, $salida, $devolver);
-
+    
+        // Verificar si el correo se envió correctamente
         if ($devolver === 0) {
             echo 'Correo enviado correctamente.';
         } else {
-        echo 'Error al enviar el correo.';
-        }   
+            echo 'Error al enviar el correo.';
+        }
     }
+    
 }
