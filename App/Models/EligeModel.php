e792bc0145f1763b904b8914c4229e908a91edc4
Ismael Begara <isbergara1@gmail.com>
2024-10-01 23:46:21 -0300

    Models de Pago

---
+++ App/Models/EligeModel.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Models;
+
+class EligeModel
+{
+    private $nroDocumento;
+    private $tipoDocumento;
+    private $idPago;
+    private $fechaPago;
+    private $nombrePlan;
+    public function __construct($nroDocumento, $tipoDocumento, $idPago, $fechaPago, $nombrePlan)
+    {
+        $this->nroDocumento = $nroDocumento;
+        $this->tipoDocumento = $tipoDocumento;
+        $this->idPago = $idPago;
+        $this->fechaPago = $fechaPago;
+        $this->nombrePlan = $nombrePlan;
+    }
+    public function getNroDocumento()
+    {
+        return $this->nroDocumento;
+    }
+    public function getTipoDocumento()
+    {
+        return $this->tipoDocumento;
+    }
+    public function getIdPago()
+    {
+        return $this->idPago;
+    }
+    public function getFechaPago()
+    {
+        return $this->fechaPago;
+    }
+    public function getNombrePlan()
+    {
+        return $this->nombrePlan;
+    }
+    public function setNroDocumento($nroDocumento)
+    {
+        $this->nroDocumento = $nroDocumento;
+    }
+    public function setTipoDocumento($tipoDocumento)
+    {
+        $this->tipoDocumento = $tipoDocumento;
+    }
+    public function setIdPago($idPago)
+    {
+        $this->idPago = $idPago;
+    }
+    public function setFechaPago($fechaPago)
+    {
+        $this->fechaPago = $fechaPago;
+    }
+    public function setNombrePlan($nombrePlan)
+    {
+        $this->nombrePlan = $nombrePlan;
+    }
+
+}
